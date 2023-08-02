27eac506402be4ea7298ebc51083f829a42f876d
ZhuB <85112881+LittleDragoon@users.noreply.github.com>
2023-08-02 18:41:38 +0200

    form UI

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,12 @@
 export default function Home() {
   return (
     <div className="flex flex-col items-center bg-gradient-to-r from-[#000428] to-[#004e92] min-h-screen">
-      <div className="font-semibold text-4xl py-4 font-bold text-[#b92b27] bg-gradient-to-r bg-clip-text text-transparent">
+      <div className="font-semibold text-4xl py-4 font-bold text-gray-300 text-transparent">
         Welcome to your TO DO list
       </div>
-      <div className="flex flex-col gap-y-3 w-full items-center pt-4">
+      <div className="flex flex-col gap-y-3 w-2/5 items-center pt-4">
         <input
-          className="w-2/5 rounded-sm text-gray-400 border border-gray-400 px-2 py-2 bg-transparent"
+          className="w-full rounded-sm text-white border border-gray-400 px-2 py-2 bg-transparent"
           autoFocus={true}
           type="text"
           id="anime-input"
@@ -15,15 +15,23 @@ export default function Home() {
           onChange={() => {}}
         />
         <textarea
-          className="w-2/5 rounded-sm text-gray-400 border border-gray-400 px-2 py-2 bg-transparent min-h-[40px]"
+          className="w-full rounded-sm text-white border border-gray-400 px-2 py-2 bg-transparent min-h-[40px]"
           id="description-id"
           name="description-name"
           placeholder="Enter a description"
           rows="4"
           cols="50"
         />
+        <select
+          className="w-full rounded-sm text-white border border-gray-400 px-2 py-2 bg-transparent"
+          name="pets"
+          id="pet-select"
+        >
+          <option value="">Pending ⌛</option>
+          <option value="dog">Completed ✅</option>
+        </select>
         <button
-          className="w-2/5 color-gray-400 bg-gradient-to-tr from-[#000428] to-[#004e92] rounded-sm px-2 text-bold text-center"
+          className="w-full color-gray-400 bg-gradient-to-tr from-[#000428] to-[#004e92] rounded-sm px-2 text-bold text-center"
           type="button"
           name="get-anime"
           onClick={() => {}}
