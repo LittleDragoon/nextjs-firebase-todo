bc8599e671acf0f2ed5d3fc98fe7718b892fc43e
ZhuB <85112881+LittleDragoon@users.noreply.github.com>
2023-11-13 11:10:57 +0100

    minor changes

--- firebase/index.js
+++ firebase/index.js
@@ -2,17 +2,15 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAbt8jXGQlAE-l_mqYgpVBgZHJBGQRma4s",
-  authDomain: "nextjs-firebase-todo-26729.firebaseapp.com",
-  projectId: "nextjs-firebase-todo-26729",
-  storageBucket: "nextjs-firebase-todo-26729.appspot.com",
-  messagingSenderId: "394918360679",
-  appId: "1:394918360679:web:677223f51ac978750b86c2",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
